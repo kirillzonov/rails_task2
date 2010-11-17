b423c1d2bbdea3bb7f070856c5915b30faa628b1
Kirill Zonov <graffzon@gmail.com>
2010-11-17 18:17:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTask2::Application.config.secret_token = 'bc96a73bc228cd9152cefdf1a3cb2fe16bd7a6106ee2e50a3380f323181f2cc99ed7acebed8c33f5f7475c8c0f374fd8dec989d28aeb8db59a83618e3d000a3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsTask2::Application.config.session_store :cookie_store, :key => '_rails_task2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# RailsTask2::Application.config.session_store :active_record_store
