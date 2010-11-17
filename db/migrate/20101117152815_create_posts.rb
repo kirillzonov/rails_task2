52ec87fce77fafb114b50e55a3dbdad0276e5bb8
Kirill Zonov <graffzon@gmail.com>
2010-11-17 18:33:49 +0300

    scaffolding posts

---
+++ db/migrate/20101117152815_create_posts.rb
@@ -0,0 +1,17 @@
+class CreatePosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.references :category
+      t.string :theme
+      t.text :content
+      t.string :source
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
