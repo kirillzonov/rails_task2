be5d715f8222be949a509b9c08b81406b2eee41a
Kirill Zonov <graffzon@gmail.com>
2010-11-17 23:42:29 +0300

    changed seeds and table width

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+Post.delete_all
+Category.delete_all
+
+(1..5).to_a.each do |index|
+    category = Category.create(:name => 'category_' + index.to_s)
+
+      (1..10).to_a.each do |post_index|
+            category.posts.create(
+                  :theme => "some_theme #{rand(100)}",
+                  :source => 'some_source',
+                  :tags => "tag" * rand(10),
+                  :content => "Some news in category  #{category.name} and about #{post_index}" * 8
+                )
+                  end
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
