bf3434d9096ab8d9e9545e36ea808cacd1541e72
Kirill Zonov <graffzon@gmail.com>
2010-11-21 00:42:30 +0300

    added where to posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 5)
+    @posts = Post.where("rate>-1").paginate(:page => params[:page], :per_page => 5)
     @user_id = session[:user_id]
     if @user_id
       @user_email = User.find(@user_id).email
