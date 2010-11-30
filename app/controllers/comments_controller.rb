3b6d7291b36357e09820e64613092dbea63cc8bc
Kirill Zonov <graffzon@gmail.com>
2010-11-30 13:13:37 +0300

    added partials for comments and ajax for them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create( params[:comment] )
-    redirect_to post_path( @post )
+    #redirect_to post_path( @post )
+    
+    respond_to do |format|
+      format.js
+    end
   end
 end
